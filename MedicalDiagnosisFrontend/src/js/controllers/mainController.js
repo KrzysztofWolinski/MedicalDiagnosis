a42d0cbe80f89c734a91ca5b2926c82e6c0e559f
Krzysiek <krzysztof.wolinski@cohesiva.com>
2014-11-04 17:25:09 +0100

    Redesigned navigation bar Added multi notifications (infoList in $rootScope)

--- MedicalDiagnosisFrontend/src/js/controllers/mainController.js
+++ MedicalDiagnosisFrontend/src/js/controllers/mainController.js
@@ -5,24 +5,26 @@ angular.module('medicalDiagnosis')
         '$rootScope',
         function($rootScope) {
 
+            $rootScope.infoList = [];
+
             $rootScope
                 .$on('$stateChangeSuccess',
                     function(event, toState, toParams, fromState, fromParams) {
-                        $rootScope.info = {
+                        $rootScope.infoList.push({
                         	'display' : true,
                             'title': 'Home',
                             'contents': 'You have entered state ' + toState.name,
                             'style': 'alert-success'
-                        };
+                        });
                     })
             $rootScope.$on('$stateNotFound',
                 function(event, unfoundState, fromState, fromParams) {
-                    $rootScope.info = {
+                    $rootScope.infoList.push({
                         	'display' : true,
                             'title': 'Error!',
                             'contents': 'There was a problem entering ' + unfoundState.to,
                             'style': 'alert-danger'
-                        };
+                        });
                 });
 
         }
