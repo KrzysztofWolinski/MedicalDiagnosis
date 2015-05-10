5b4c2f6ed87649be4325960565dcd8d8e5f170ba
Krzysiek <krzysztof.wolinski@cohesiva.com>
2015-05-10 13:11:59 +0200

    Working controller

---
+++ MedicalDiagnosis/medical-diagnosis-integration/src/main/java/com/bodziowagh/config/RootConfig.java
@@ -0,0 +1,12 @@
+package com.bodziowagh.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.bodziowagh.controller")
+class RootConfig extends WebMvcConfigurerAdapter {
+}
