5d174e17cf2307f759948047b6b9603ef5416d45
Krzysiek <krzysztof.wolinski@cohesiva.com>
2015-05-10 15:31:21 +0200

    Working internal service. Issue with external service in different project remains

--- MedicalDiagnosis/medical-diagnosis-integration/src/main/java/com/bodziowagh/controller/SampleController.java
+++ MedicalDiagnosis/medical-diagnosis-integration/src/main/java/com/bodziowagh/controller/SampleController.java
@@ -1,17 +1,25 @@
-package com.meddiag.controller;
+package com.bodziowagh.controller;
+
+import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-public class SampleController {
+import com.bodziowagh.sample.SampleService;
 
+@Controller
+class SampleController {
+
+	@Inject
+	SampleService service;
+	
+    @SuppressWarnings("SameReturnValue")
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
-    public String showIndex() {
-        return "Hello world";
+    public String showIndex() {  	
+        return service.sampleValue();
     }
 
 }
---
+++ MedicalDiagnosis/medical-diagnosis-integration/src/main/java/com/bodziowagh/sample/SampleService.java
@@ -0,0 +1,9 @@
+package com.bodziowagh.sample;
+
+public class SampleService {
+	
+	public String sampleValue() {
+		return "Hello there! Its sample service!";
+	}
+	
+}
