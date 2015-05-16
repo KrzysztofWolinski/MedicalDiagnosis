c34f463442bc05bc963e8149beb8ffd70a419d11
Krzysiek <krzysztof.wolinski@cohesiva.com>
2015-05-16 12:39:25 +0200

    Added Jackson as a dependency. Now objects can be, and are returned by the controller.

--- MedicalDiagnosis/medical-diagnosis-integration/src/main/java/com/medica/integration/controller/SampleController.java
+++ MedicalDiagnosis/medical-diagnosis-integration/src/main/java/com/medica/integration/controller/SampleController.java
@@ -2,16 +2,15 @@ package com.medica.integration.controller;
 
 import javax.inject.Inject;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.medica.core.domain.SampleObject;
 import com.medica.core.service.SampleService;
 
-@Controller
+@RestController
 @RequestMapping("/")
 class SampleController {
 	
@@ -19,25 +18,21 @@ class SampleController {
 	private SampleService sampleService;
 	
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
     public String sample() {  	
         return "Hello, it's SampleController";
     }
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
-    @ResponseBody
     public String sampleTest() {
     	return sampleService.sampleValue();
     }
     
     @RequestMapping(value = "/{value}", method = RequestMethod.GET)
-    @ResponseBody
     public String sampleValueWithParam(@PathVariable String value) {
     	return "You have entered " + value;
     }
     
     @RequestMapping(value = "/object", method = RequestMethod.GET)
-    @ResponseBody
     public SampleObject sampleObject() {
     	return sampleService.sampleObject();
     }
