964e86874bcd75568ab1376128f5995b82186264
Krzysiek <krzysztof.wolinski@cohesiva.com>
2015-11-03 19:09:10 +0100

    Project migration, frontend moved to different repo

--- MedicalDiagnosisFrontend/src/js/Config.js
+++
@@ -1,7 +0,0 @@
-angular.module('myApp')
-.config(['$httpProvider',
-	function($httpProvider) {
-		'use strict';
-		$httpProvider.defaults.timeout = 5000;
-	}
-	]);
--- MedicalDiagnosisFrontend/src/js/controllers/homeController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('medicalDiagnosis')
-	.controller('homeController', [
-		'$rootScope',
-		function($rootScope) {
-			
-		}
-	]);
